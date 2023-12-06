4b1b5c1c32edaf271efdc9397037fe1d57129466
Phil Splettstoeszer <stoeszer09@gmail.com>
2023-12-06 17:17:47 -0600

    add Auth0

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
-import Home from './components/Home';
-import Income from './components/Income';
-import Expenses from './components/Expenses';
+import Home from './pages/Home';
+import Income from './pages/Income';
+import Expenses from './pages/Expenses';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
+        <LoginButton />
+        <LogoutButton />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/expenses" element={<Income />} />
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+  domain="dev-j-ihoupd.us.auth0.com"
+  clientId='1fKsYny4zg229o09oxIPuu461C0mPEO3'
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
