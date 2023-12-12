f152b466162fe152a4de2e492afc37dbe02cd64c
Phil Splettstoeszer <stoeszer09@gmail.com>
2023-12-12 15:27:08 -0600

    update name and budget to backend

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,25 +2,43 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useAuth0, User } from "@auth0/auth0-react";
 import { useState } from 'react';
+import { Button, FormControl, Input, InputAdornment, InputLabel } from '@mui/material';
+
+const backend_URL = process.env.REACT_APP_BACKEND_URL as string
 
 function Profile() {
   const [name, setName] = useState('');
-  const [budget, setBudget] = useState('100');
+  const [budget, setBudget] = useState(100);
   const { user, isAuthenticated, isLoading } = useAuth0<User>();
-
+  
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setName(e.target.value);
   };
   
   const handleBudgetChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setBudget(e.target.value);
+    setBudget(parseInt(e.target.value));
   };
   
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   // Call the onUpdateProfile function with the updated data
-  //   onUpdateProfile({ nickname, budget });
-  // };
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    console.log(backend_URL + '/users/monthly-budgets')
+    try {
+      const response = await fetch(backend_URL + '/users/monthly-budgets', {
+        method: 'PUT', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, budget, user }),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to update profile');
+      }
+      const data = await response.json();
+      console.log(data)
+    } catch (error) {
+      console.error('Error updating profile:', error);
+    }
+  };
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -31,17 +49,41 @@ function Profile() {
       {isAuthenticated && user && (
         <div>
           <h2>{user.nickname}</h2>
-          <Box
-            component="form"
-            sx={{
-              '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField id="standard-basic" onChange={handleNameChange} defaultValue={`${user.nickname}`} label="Name" variant="standard" />
-            <TextField id="standard-basic" onChange={handleBudgetChange} defaultValue={`${budget}`} label="Montly Budget" variant="standard" />
-          </Box>
+          <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+            <Box
+              component="form"
+              sx={{
+                '& > :not(style)': { m: 1, width: '25ch' },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="standard-basic"
+                onChange={handleNameChange}
+                defaultValue={`${user.nickname}`}
+                label="Name"
+                variant="standard"
+              />
+              <br />
+              <InputLabel htmlFor="standard-adornment-amount">
+                Amount
+              </InputLabel>
+              <Input
+                id="standard-adornment-amount"
+                onChange={handleBudgetChange}
+                startAdornment={
+                  <InputAdornment position="start">$</InputAdornment>
+                }
+              />
+            </Box>
+            {user.nickname !== name || budget !== budget ? (
+              <Button onClick={handleSubmit} sx={{ width: 25 }} variant="contained">Submit</Button>
+            ) : (
+              <Button onClick={handleSubmit} sx={{ width: 25 }} variant="outlined">Submit</Button>
+            )}
+          </FormControl>
+          {budget}
         </div>
       )}
     </div>
